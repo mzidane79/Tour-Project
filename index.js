03d672953ca6d11a17ef06b4396b6acc8259b4df
mzidane79 <82753479+mzidane79@users.noreply.github.com>
2023-12-17 20:28:05 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Function to parse and display user information
+    function displayUserInfo() {
+        // Get the ID token from the URL
+        const idToken = new URLSearchParams(window.location.hash).get('id_token');
+
+        if (idToken) {
+            // Parse the ID token
+            const parsedToken = JSON.parse(atob(idToken.split('.')[1]));
+
+            // Display user information
+            const userInfoContainer = document.getElementById('userInfo');
+            userInfoContainer.innerHTML = `
+                <h2>User Information</h2>
+                <p>Name: ${parsedToken.name}</p>
+                <p>Email: ${parsedToken.email}</p>
+                <p>Sub: ${parsedToken.sub}</p>
+            `;
+        } else {
+            console.error('ID token not found in the URL.');
+        }
+    }
+
+    // Call the function to display user information
+    displayUserInfo();
+});
