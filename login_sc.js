155691a11964de50a2ad63fb4f6faf580bc95e19
mzidane79 <82753479+mzidane79@users.noreply.github.com>
2023-12-17 20:10:21 +0200

    Create login_sc.js

---
+++ login_sc.js
@@ -0,0 +1,18 @@
+// Event listener for the button click
+document.addEventListener('DOMContentLoaded', function () {
+    // OktaAuth is now defined
+    // UPDATE THESE FOR YOUR OKTA TENANT
+    var baseOktaURL = "https://dev-16407622.okta.com";
+    var appClientID = "0oadvrpixrp4jsDvz5d7";
+
+    // Bootstrap the AuthJS Client
+    const authClient = new OktaAuth({ url: baseOktaURL, clientId: appClientID, redirectUri: "http://127.0.0.1:5500/index.html", issuer: baseOktaURL, scope: ['openid', 'profile', 'email'] });
+
+    // Event listener for the button click
+    document.getElementById('loginButton').addEventListener('click', function () {
+        // Redirect to Okta for authentication
+        authClient.token.getWithRedirect({
+            responseType: ['id_token']
+        });
+    });
+});
