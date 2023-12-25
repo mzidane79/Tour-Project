9a9a8c0a95d861adcfc3f6b84c61e8fe8c9f070d
mzidane79 <82753479+mzidane79@users.noreply.github.com>
2023-12-25 11:08:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
-console.log('index.js script executed');
+// index.js
 
 document.addEventListener('DOMContentLoaded', function () {
-    // Function to parse and display user information
-    function displayUserInfo() {
-        // Get the ID token from the URL
-        const idToken = new URLSearchParams(window.location.hash).get('id_token');
+    // Function to get user information from Okta
+    function getUserInfo() {
+        // Initialize the OktaAuth client
+        const authClient = new OktaAuth({
+            url: "https://dev-16407622.okta.com", // Update with your Okta URL
+            clientId: "0oadvrpixrp4jsDvz5d7", // Update with your Okta client ID
+            redirectUri: "https://mzidane79.github.io/Tour-Project/index.html", // Update with your redirect URI
+            issuer: "https://dev-16407622.okta.com", // Update with your Okta issuer
+            scope: ['openid', 'profile', 'email']
+        });
 
-        if (idToken) {
-            // Parse the ID token
-            const parsedToken = JSON.parse(atob(idToken.split('.')[1]));
-
-            // Display user information
-            const userInfoContainer = document.getElementById('userInfo');
-            userInfoContainer.innerHTML = `
-                <h2>User Information</h2>
-                <p>Name: ${parsedToken.name}</p>
-                <p>Email: ${parsedToken.email}</p>
-                <p>Sub: ${parsedToken.sub}</p>
-            `;
-        } else {
-            console.error('ID token not found in the URL.');
-        }
+        // Get the user information
+        authClient.token.getUserInfo()
+            .then(userInfo => {
+                // Display user information in the userInfo div
+                const userInfoDiv = document.getElementById('userInfo');
+                userInfoDiv.innerHTML = `
+                    <p><strong>User ID:</strong> ${userInfo.sub}</p>
+                    <p><strong>Email:</strong> ${userInfo.email}</p>
+                    <p><strong>Name:</strong> ${userInfo.name}</p>
+                `;
+            })
+            .catch(error => {
+                console.error('Error fetching user information:', error);
+            });
     }
 
-    // Call the function to display user information
-    displayUserInfo();
+    // Event listener for the button click
+    document.getElementById('loginButton').addEventListener('click', function () {
+        // Redirect to Okta for authentication
+        authClient.token.getWithRedirect({
+            responseType: ['id_token']
+        });
+    });
 
+    // Event listener for the button click to get user information
+    document.getElementById('getUserInfoButton').addEventListener('click', getUserInfo);
 });
