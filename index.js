1a0598364828e6fa9c5dbbe90c917aa0236d747f
mzidane79 <82753479+mzidane79@users.noreply.github.com>
2023-12-25 16:07:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,17 @@ const authClient = new OktaAuth({
     redirectUri: "https://mzidane79.github.io/Tour-Project/index.html",
     issuer: "https://dev-16407622.okta.com",
     scope: ['openid', 'profile', 'email']
+    console.log('defining the const');
 });
 
 // Initialize the Engagement Messenger
-SN_CSM_EC.init({
+SN_CSM_EC.init(
+    {
     moduleID: "https://dev175885.service-now.com/#6246e8d6474331104c5e379bd36d43d7",
     loadFeature: SN_CSM_EC.loadEMFeature()
-});
+    }
+    console.log('defining the INIT');
+            );
 
 // Function to display user information
 console.log('Index JS start the user function');
@@ -23,8 +27,7 @@ console.log('Index JS inside user function 1');
     // Check if the user is authenticated
     if (authClient.tokenManager.get('idToken')) {
         const idToken = authClient.tokenManager.get('idToken');
-console.log('Index JS inside user function 2');
-        console.log(idToken);
+
         // Display user information
         userInfoContainer.innerHTML = `<p>User Info:</p>
                                        <p>Name: ${idToken.claims.name}</p>
