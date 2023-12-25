3b17b6854e8af6abe4a566a4dcf08bb3d67d297b
mzidane79 <82753479+mzidane79@users.noreply.github.com>
2023-12-25 19:54:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('Hello 1 from index JS');
+console.log('Hello from index JS');
 
 // Bootstrap the AuthJS Client
 const authClient = new OktaAuth({
@@ -16,26 +16,20 @@ SN_CSM_EC.init({
 });
 
 // Function to display user information
-console.log('Index JS start the user function');
-
 function displayUserInfo() {
-    // Check if the DOM is fully loaded
-    if (document.readyState === 'complete') {
-        const userInfoContainer = document.getElementById('userInfo');
-        console.log('Index JS inside user function 1');
+    const userInfoContainer = document.getElementById('userInfo');
 
-        // Check if the user is authenticated
-        if (authClient.tokenManager.get('idToken')) {
-            const idToken = authClient.tokenManager.get('idToken');
+    // Check if the user is authenticated
+    if (authClient.tokenManager.get('idToken')) {
+        const idToken = authClient.tokenManager.get('idToken');
 
-            // Display user information
-            userInfoContainer.innerHTML = `<p>User Info:</p>
-                                           <p>Name: ${idToken.claims.name}</p>
-                                           <p>Email: ${idToken.claims.email}</p>`;
-        } else {
-            userInfoContainer.innerHTML = '<p>User is not authenticated. Please login first.</p>';
-            console.log('ERROR generating token');
-        }
+        // Display user information
+        userInfoContainer.innerHTML = `<p>User Info:</p>
+                                       <p>Name: ${idToken.claims.name}</p>
+                                       <p>Email: ${idToken.claims.email}</p>`;
+    } else {
+        userInfoContainer.innerHTML = '<p>User is not authenticated. Please login first.</p>';
+        console.log('Error generating token');
     }
 }
 
@@ -43,9 +37,4 @@ function displayUserInfo() {
 displayUserInfo();
 
 // Event listener for the "Get User Info" button click
-document.addEventListener('DOMContentLoaded', function () {
-    const getUserInfoButton = document.getElementById('getUserInfoButton');
-    if (getUserInfoButton) {
-        getUserInfoButton.addEventListener('click', displayUserInfo);
-    }
-});
+document.getElementById('getUserInfoButton').addEventListener('click', displayUserInfo);
