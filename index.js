c96aee83851fb7adf35016355f0f4e0fc08b56bb
mzidane79 <82753479+mzidane79@users.noreply.github.com>
2023-12-25 11:15:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,32 +13,19 @@ SN_CSM_EC.init({
     loadFeature: SN_CSM_EC.loadEMFeature()
 });
 
-// Event listener for the button click
-document.addEventListener('DOMContentLoaded', function () {
-    // Function to get user information from Okta
-    function getUserInfo() {
-        const userInfoContainer = document.getElementById('userInfo');
+// Event listener for the "Get User Info" button click
+document.getElementById('getUserInfoButton').addEventListener('click', function () {
+    const userInfoContainer = document.getElementById('userInfo');
 
-        // Check if the user is authenticated
-        if (authClient.token.hasTokensInUrl()) {
-            // Parse the tokens from the URL
-            authClient.token.parseFromUrl()
-                .then(data => {
-                    const idToken = data.tokens.idToken;
+    // Check if the user is authenticated
+    if (authClient.tokenManager.get('idToken')) {
+        const idToken = authClient.tokenManager.get('idToken');
 
-                    // Display user information
-                    userInfoContainer.innerHTML = `<p>User Info:</p>
-                                                   <p>Name: ${idToken.claims.name}</p>
-                                                   <p>Email: ${idToken.claims.email}</p>`;
-                })
-                .catch(error => {
-                    console.error('Error parsing tokens:', error);
-                });
-        } else {
-            userInfoContainer.innerHTML = '<p>User is not authenticated. Please login first.</p>';
-        }
+        // Display user information
+        userInfoContainer.innerHTML = `<p>User Info:</p>
+                                       <p>Name: ${idToken.claims.name}</p>
+                                       <p>Email: ${idToken.claims.email}</p>`;
+    } else {
+        userInfoContainer.innerHTML = '<p>User is not authenticated. Please login first.</p>';
     }
-
-    // Event listener for the "Get User Info" button click
-    document.getElementById('getUserInfoButton').addEventListener('click', getUserInfo);
 });
