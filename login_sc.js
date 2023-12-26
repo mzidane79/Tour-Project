2eded134b667645bebada392a7bbb34e7c3f3c31
mzidane79 <82753479+mzidane79@users.noreply.github.com>
2023-12-26 15:39:07 +0200

    Update login_sc.js

--- login_sc.js
+++ login_sc.js
@@ -1,19 +1,15 @@
 console.log('Hi From login JS');
-// Event listener for the button click
 document.addEventListener('DOMContentLoaded', function () {
-    // OktaAuth is now defined
-    // UPDATE THESE FOR YOUR OKTA TENANT
     var baseOktaURL = "https://dev-16407622.okta.com";
     var appClientID = "0oadvrpixrp4jsDvz5d7";
 
-    // Bootstrap the AuthJS Client
     const authClient = new OktaAuth({ url: baseOktaURL, clientId: appClientID, redirectUri: "https://mzidane79.github.io/Tour-Project/index.html", issuer: baseOktaURL, scope: ['openid', 'profile', 'email'] });
-    console.log(authClient);
-    // Event listener for the button click
+
     document.getElementById('loginButton').addEventListener('click', function () {
         // Redirect to Okta for authentication
         authClient.token.getWithRedirect({
             responseType: ['id_token']
         });
     });
+console.log(authClient.token);
 });
