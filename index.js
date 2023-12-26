34c5b2600dd631b21c44db5ab5ac38ccb25e711d
mzidane79 <82753479+mzidane79@users.noreply.github.com>
2023-12-26 08:49:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,29 @@
-console.log('Hello 1 from index JS');
+console.log('index.js script executed');
 
-// Bootstrap the AuthJS Client
-const authClient = new OktaAuth({
-    url: "https://dev-16407622.okta.com",
-    clientId: "0oadvrpixrp4jsDvz5d7",
-    redirectUri: "https://mzidane79.github.io/Tour-Project/index.html",
-    issuer: "https://dev-16407622.okta.com",
-    scope: ['openid', 'profile', 'email']
-});
+document.addEventListener('DOMContentLoaded', function () {
+    // Function to parse and display user information
+    function displayUserInfo() {
+        // Get the ID token from the URL
+        const idToken = new URLSearchParams(window.location.hash).get('id_token');
 
-// Initialize the Engagement Messenger
-SN_CSM_EC.init({
-    moduleID: "https://dev175885.service-now.com/#6246e8d6474331104c5e379bd36d43d7",
-    loadFeature: SN_CSM_EC.loadEMFeature()
-});
+        if (idToken) {
+            // Parse the ID token
+            const parsedToken = JSON.parse(atob(idToken.split('.')[1]));
 
-// Function to display user information
-console.log('Index JS start the user function');
-
-function displayUserInfo() {
-    const userInfoContainer = document.getElementById('userInfo');
-
-    // Check if the user is authenticated
-    if (authClient.tokenManager.get('idToken')) {
-        const idToken = authClient.tokenManager.get('idToken');
-
-        // Check if idToken and its claims are defined
-        if (idToken && idToken.claims) {
             // Display user information
-            userInfoContainer.innerHTML = `<p>User Info:</p>
-                                           <p>Name: ${idToken.claims.name || 'N/A'}</p>
-                                           <p>Email: ${idToken.claims.email || 'N/A'}</p>`;
+            const userInfoContainer = document.getElementById('userInfo');
+            userInfoContainer.innerHTML = `
+                <h2>User Information</h2>
+                <p>Name: ${parsedToken.name}</p>
+                <p>Email: ${parsedToken.email}</p>
+                <p>Sub: ${parsedToken.sub}</p>
+            `;
         } else {
-            userInfoContainer.innerHTML = '<p>User claims are not available.</p>';
-            console.log('Error: User claims are not available.');
+            console.error('ID token not found in the URL.');
         }
-    } else {
-        userInfoContainer.innerHTML = '<p>User is not authenticated. Please login first.</p>';
-        console.log('Error: User is not authenticated.');
     }
-}
 
-console.log('trying to get the token automatically');
-console.log(authClient.tokenManager.getTokens());
+    // Call the function to display user information
+    displayUserInfo();
 
-// Automatically display user information on page load
-displayUserInfo();
-
-// Event listener for the "Get User Info" button click
-document.getElementById('getUserInfoButton').addEventListener('click', displayUserInfo);
+});
