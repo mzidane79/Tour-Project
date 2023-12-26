3511eedfdeab9eb233fbbae2ddaa516941ebc358
mzidane79 <82753479+mzidane79@users.noreply.github.com>
2023-12-26 09:43:49 +0200

    Update login.js

--- login.js
+++ login.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const authClient = new OktaAuth({
         url: baseOktaURL,
         clientId: appClientID,
-        redirectUri: "https://your-redirect-uri.com",
+        redirectUri: "https://mzidane79.github.io/Tour-Project/engagement_messenger.html",
         issuer: baseOktaURL,
         scope: ['openid', 'profile', 'email']
     });
